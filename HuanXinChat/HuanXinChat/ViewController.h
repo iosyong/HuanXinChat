977a6bf879e068a3646acd491ef8c97f1bd3e327
yong <290418001@qq.com>
2015-09-24 13:22:51 +0800

    HuanXinChatDemo

---
+++ HuanXinChat/HuanXinChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HuanXinChat
+//
+//  Created by yong on 15/9/24.
+//  Copyright (c) 2015年 iosYong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
